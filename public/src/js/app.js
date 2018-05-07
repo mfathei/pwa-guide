8ab7c17e146bf1fed067bc483e0c2a27d28c863b
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-07 23:25:58 +0200

    get request with fetch

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -2,5 +2,17 @@ if('serviceWorker' in navigator){
 	navigator.serviceWorker.register('/sw.js')
 	.then(function(){
 		console.log('service worker registered!');
+	}).catch(function(err){
+		console.log(err);
 	});
 }
+
+
+fetch('https://httpbin.org/ip').
+then(function(response){
+	return response.json();
+}).then(function(data){
+	console.log(data);
+}).catch(function(err){
+	console.log(err);
+})
