5d22ead1874c1c1766e7c899e35186e8a457a5cb
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-07 23:40:56 +0200

    post request in fetch and allow CORS

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -7,7 +7,7 @@ if('serviceWorker' in navigator){
 	});
 }
 
-
+// GET
 fetch('https://httpbin.org/ip').
 then(function(response){
 	return response.json();
@@ -15,4 +15,22 @@ then(function(response){
 	console.log(data);
 }).catch(function(err){
 	console.log(err);
-})
+});
+
+// POST
+fetch('https://httpbin.org/post', {
+	method: 'POST',
+	headers: {
+		'Content-type': 'application/json',
+		'Accept': 'application/json',
+	},
+	mode: "cors",
+	body: JSON.stringify({message: "This works."})
+}).
+then(function(response){
+	return response.json();
+}).then(function(data){
+	console.log(data);
+}).catch(function(err){
+	console.log(err);
+});
