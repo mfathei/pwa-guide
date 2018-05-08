9f96f90115c6fbd03d66fbe6615ee6657ca8dbd5
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-08 10:06:55 +0200

    handling erroras

--- public/sw.js
+++ public/sw.js
@@ -42,6 +42,9 @@ self.addEventListener('fetch', function(event){
  					cache.put(event.request.url, res.clone());
  					return res;
  				})
+		 	})
+		 	.catch(function(err){
+		 		
 		 	});
 		 })
 	);
