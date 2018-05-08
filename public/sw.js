f018654976aed2d1a7cd1c21ef206c5bcedf46b5
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-08 15:41:41 +0200

    cleaning Trimming the cache

--- public/sw.js
+++ public/sw.js
@@ -27,6 +27,19 @@ function isInArray(string, arr){
 	return false;
 }
 
+function trimCache(cacheName, maxItems){
+	caches.open(cacheName)
+	.then(function(cache){
+		return cache.keys()
+		.then(function(keys){
+			if(keys.length > maxItems){
+				cache.delete(keys[0])
+				.then(trimCache(cacheName, maxItems));
+			}
+		});
+	});
+}
+
 self.addEventListener('install', function(event){
 	console.log('[Service Worker] Installing service worker...', event);
 	event.waitUntil(caches.open(CACHE_STATIC_NAME).then(function(cache){
@@ -87,6 +100,7 @@ self.addEventListener('fetch', function(event){
 			.then(function(cache){
 				return fetch(event.request)
 				.then(function(res){
+					trimCache(CACHE_DYNAMIC_NAME, 3);
 					cache.put(event.request, res.clone());
 					return res;
 				})
@@ -109,6 +123,7 @@ self.addEventListener('fetch', function(event){
 		 		.then(function(res){
 		 			return caches.open(CACHE_DYNAMIC_NAME)
 	 				.then(function(cache){
+	 					trimCache(CACHE_DYNAMIC_NAME, 3);
 	 					cache.put(event.request.url, res.clone());
 	 					return res;
 	 				})
