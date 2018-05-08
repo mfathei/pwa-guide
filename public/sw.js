c4a6d53898269f4b959c7794cc33c8d5344b54e8
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-08 09:42:59 +0200

    cache multiple files with addAll

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,21 @@ self.addEventListener('install', function(event){
 	console.log('[Service Worker] Installing service worker...', event);
 	event.waitUntil(caches.open('static').then(function(cache){
 			console.log('[Service Worker] Precaching the App Shell...');
-			cache.add('/src/js/app.js');
+			cache.addAll([
+				'/',
+				'/index.html',
+				'/src/js/feed.js',
+				'/src/js/promise.js',
+				'/src/js/fetch.js',
+				'/src/js/app.js',
+				'/src/js/material.min.js',
+				'/src/css/feed.css',
+				'/src/css/app.css',
+				'/src/images/main-image.jpg',
+				"https://fonts.googleapis.com/css?family=Roboto:400,700",
+				"https://fonts.googleapis.com/icon?family=Material+Icons",
+				"https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
+			]);
 		})
 	);
 });
