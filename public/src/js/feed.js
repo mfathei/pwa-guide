ed6eb5af27d6e57af9ce99c4ccf70ed5933d9dd4
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-09 12:41:40 +0200

    adding the basic setup for background sync

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -2,6 +2,9 @@ var shareImageButton = document.querySelector('#share-image-button');
 var createPostArea = document.querySelector('#create-post');
 var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
 var sharedMomentsArea = document.querySelector('#shared-moments');
+var form = document.querySelector('form');
+var titleInput = document.querySelector('#title');
+var locationInput = document.querySelector('#location');
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
@@ -122,3 +125,12 @@ if('indexedDB' in window){
 		}
 	);
 }
+
+form.addEventListener('submit', function(event){
+	event.preventDefault();
+	if(titleInput.value.trim() === '' || locationInput.value.trim() === ''){
+		return;
+	}
+
+	closeCreatePostModal();
+});
