283e42cc990924365a50e37c82d527be8b2131de
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-18 17:04:39 +0200

    expanding dynamic caching

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,9 +2,21 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 
 workbox.precaching.suppressWarnings();
 
-workbox.routing.registerRoute(/.*(?:googleapis|gstatis)*.$/,
-    workbox.strategies.cacheFirst({
+workbox.routing.registerRoute(/.*(?:googleapis|gstatic)\.com.*$/,
+    workbox.strategies.staleWhileRevalidate({
         cacheName: 'google-fonts'
+    })
+);
+
+workbox.routing.registerRoute('https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'material-css'
+    })
+);
+
+workbox.routing.registerRoute(new RegExp('/photos/'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'post-images'
     })
 );
 
@@ -71,7 +83,7 @@ workbox.precaching.precacheAndRoute([
   },
   {
     "url": "sw-base.js",
-    "revision": "85a494011fea02faee9c987602c2630c"
+    "revision": "3b34e2237eb420551ff27b01d971f97d"
   },
   {
     "url": "sw.js",
--- public/sw-base.js
+++ public/sw-base.js
@@ -2,9 +2,21 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 
 workbox.precaching.suppressWarnings();
 
-workbox.routing.registerRoute(/.*(?:googleapis|gstatis)*.$/,
+workbox.routing.registerRoute(/.*(?:googleapis|gstatic)\.com.*$/,
     workbox.strategies.staleWhileRevalidate({
         cacheName: 'google-fonts'
+    })
+);
+
+workbox.routing.registerRoute('https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'material-css'
+    })
+);
+
+workbox.routing.registerRoute(new RegExp('/photos/'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'post-images'
     })
 );
 
