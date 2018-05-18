6b017d667e9ecfeb9684d7ebd22516e72a2226cf
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-05-18 18:26:18 +0200

    Handling background sync and push notifications

--- public/sw-base.js
+++ public/sw-base.js
@@ -1,4 +1,5 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+// importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+importScripts("workbox-sw.js");
 importScripts('/src/js/idb.js');
 importScripts('/src/js/utility.js');
 
@@ -76,3 +77,102 @@ workbox.routing.registerRoute(function(routeData) {
 });
 
 workbox.precaching.precacheAndRoute([], {});
+
+self.addEventListener('sync', function(event) {
+    console.log('[Service Worker] Background syncing', event);
+    if (event.tag === 'sync-new-posts') {
+        console.log('[Service Worker] Syncing new Posts');
+        event.waitUntil(
+            readAllData('sync-posts')
+            .then(function(data) {
+                console.log('sync-posts');
+                for (var dt of data) {
+                    var postData = new FormData();
+                    postData.append('id', dt.id);
+                    postData.append('title', dt.title);
+                    postData.append('location', dt.location);
+                    postData.append('rawLocationLat', dt.rawLocation.lat);
+                    postData.append('rawLocationLng', dt.rawLocation.lng);
+                    postData.append('file', dt.picture, dt.id + '.png');
+
+                    fetch('http://localhost:3000/api/posts', {
+                            method: 'POST',
+                            body: postData
+                        })
+                        .then(function(res) {
+                            console.log('Sent data', res);
+                            if (res.ok) {
+                                res.json()
+                                    .then(function(resData) {
+                                        deleteDataItem('sync-posts', resData.id);
+                                    });
+                            }
+                        })
+                        .catch(function(err) {
+                            console.log('Error while sending data', err);
+                        });
+                }
+
+            })
+        );
+    }
+});
+
+self.addEventListener('notificationclick', function(event) {
+    var notification = event.notification;
+    var action = event.action;
+
+    if (action === 'confirm') {
+        console.log('Confirm clicked');
+        notification.close();
+    } else {
+        console.log(action);
+        event.waitUntil(
+            clients.matchAll()
+            .then(function(clis) {
+                var client = clis.find(function(c) {
+                    return c.visibilityState === 'visible';
+                });
+
+                if (client) {
+                    client.navigate(notification.data.url);
+                    client.focus();
+                } else {
+                    clients.openWindow(notification.data.url);
+                }
+                notification.close();
+            })
+        );
+    }
+});
+
+self.addEventListener('notificationclose', function(event) {
+    console.log('Notification closed', event);
+});
+
+self.addEventListener('push', function(event) {
+    console.log('Push notification received', event);
+
+    var data = {
+        title: "New!",
+        content: "New content",
+        openUrl: "/"
+    };
+
+    if (event.data) {
+        data = JSON.parse(event.data.text());
+    }
+
+    var options = {
+        body: data.content,
+        icon: '/src/images/icons/app-icon-96x96.png',
+        badge: '/src/images/icons/app-icon-96x96.png',
+        data: {
+            url: data.openUrl
+        }
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
